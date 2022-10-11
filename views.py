c65bb5d42a5a5573097a3a8c23ad459d80cb0808
NurhalizaMulyanti <106473793+NurhalizaMulyanti@users.noreply.github.com>
2022-10-11 07:57:03 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    judul = "Profil"
+    judul = "Fakultas Pertanian"
 
     konteks = {
         'judul': judul
